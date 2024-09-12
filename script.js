bc605f9a7f657ef0bd9ff002d155e2f6cb0649d1
Berkay Girgin <28847330+Gerile3@users.noreply.github.com>
2024-09-12 18:01:55 +0300

    add logic for the game

--- script.js
+++ script.js
@@ -1,35 +1,50 @@
-const paintArea = document.getElementById("paint-area-container")
-const inputBtn = document.getElementById("input-btn")
-const desiredArea = document.getElementById("area-value")
+const boxContainer = document.querySelector(".box-container")
+const configBtn = document.querySelector(".input")
+
+let rowNumber = 16 // default 16 boxes on each row
+let size = 256 // default 256 boxes in total
+let opacity = 10 // default %10 opacity
 
 
-
-function drawBox(){
-
-    const div_ = document.createElement("div");
-    div_.classList.add("box")
-    div_.style.setProperty('--box-width', `${960 / desiredArea.value}px`)
-    div_.style.setProperty('--box-height', `${960 / desiredArea.value}px`)
-    paintArea.appendChild(div_)
-}
-
-function clearArea(box){
-    box.forEach(element => {
-        paintArea.removeChild(element)
-    });
-}
-
-function drawArea(){
-    let isPainted = document.querySelectorAll(".box");
-    
-    if (isPainted){
-        clearArea(isPainted)
-    }
-
-    for (let i = 0 ; i < desiredArea.value ; i++ ){
-        drawBox()
+function drawBoxes(size) {
+    let sqrtBox = Math.sqrt(size)
+    let boxSize = Math.floor((960 / sqrtBox)) + 'px'; 
+    for (let index = 0; index < size; index++) {
+        let newBox = document.createElement("div")
+        newBox.className = "box"
+        newBox.style.height = boxSize
+        newBox.style.width = boxSize
+        boxContainer.appendChild(newBox)
     }
 }
 
-inputBtn.addEventListener("click", drawArea)
+function randomColors() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
 
+    return `rgb(${r}, ${g}, ${b}, ${opacity}%)`;
+}
+
+document.addEventListener("mouseover", (event)=>{
+    if (event.target.classList.contains('box')) {
+        opacity += 0.5
+        if (opacity > 100) opacity = 100;
+        event.target.style.backgroundColor = randomColors()
+    }
+})
+
+configBtn.addEventListener("click",() => {
+    opacity = 10
+    boxContainer.innerHTML = ''; 
+    rowNumber = Number(prompt("Enter row size(e.g., 10 for a 10x10 grid max:16):"), 16);
+
+    if (isNaN(rowNumber) || rowNumber <= 0 || rowNumber > 16) {
+        rowNumber = 16; // Default value
+    }
+
+    size = Math.pow(rowNumber,2)
+    drawBoxes(size)
+})
+
+document.addEventListener("DOMContentLoaded",() => drawBoxes(size))
